84de042b388fd440196a3a5fef0e71724ccf43b4
Mariem Amari <ammeri.mariem.info@gmail.com>
2018-12-10 13:32:45 +0100

    Commit de l'exemple hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-sofrecom',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-angulr0-hello';
+  title = 'demo-angular0-hello';
+  name = 'Mariem';
 }
